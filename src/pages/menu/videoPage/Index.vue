1cff3716d74de728a5ea2101a21c7956b6b434b6
stark <784184859@qq.com>
2020-07-31 15:34:43 +0800

    视频页面

--- src/pages/menu/videoPage/Index.vue
+++ src/pages/menu/videoPage/Index.vue
@@ -1,111 +1,14 @@
 <!--
  * @Date         : 2020-07-29 16:59:02
  * @LastEditors  : HaoJie
- * @LastEditTime : 2020-07-30 17:48:40
+ * @LastEditTime : 2020-07-31 15:08:02
  * @FilePath     : \src\pages\menu\videoPage\Index.vue
 -->
 <script lang="ts">
 import { Component, Vue } from "vue-property-decorator";
 import { getModule } from "vuex-module-decorators";
 import videoPageStore from "store/modules/videoPage/videoPageStore";
-const videoData: any[] = [
-  {
-    name: "1号摄像头",
-    id: 1,
-    url:
-      "http://hls01open.ys7.com/openlive/3c3d8a4396ff49018e7a1011095a22b9.hd.m3u8"
-  },
-  {
-    name: "2号摄像头",
-    id: 2,
-    url:
-      "http://hls.open.ys7.com/openlive/7da9d05ec1df49cc89a847296d79e1a4.hd.m3u8"
-  },
-  {
-    name: "3号摄像头",
-    id: 3,
-    url:
-      "http://hls01open.ys7.com/openlive/3c3d8a4396ff49018e7a1011095a22b9.hd.m3u8"
-  },
-  {
-    name: "4号摄像头",
-    id: 4,
-    url:
-      "http://hls01open.ys7.com/openlive/3c3d8a4396ff49018e7a1011095a22b9.hd.m3u8"
-  },
-  {
-    name: "5号摄像头",
-    id: 5,
-    url:
-      "http://hls01open.ys7.com/openlive/3c3d8a4396ff49018e7a1011095a22b9.hd.m3u8"
-  },
-  {
-    name: "6号摄像头",
-    id: 6,
-    url:
-      "http://hls01open.ys7.com/openlive/3c3d8a4396ff49018e7a1011095a22b9.hd.m3u8"
-  },
-  {
-    name: "7号摄像头",
-    id: 7,
-    url:
-      "http://hls01open.ys7.com/openlive/3c3d8a4396ff49018e7a1011095a22b9.hd.m3u8"
-  },
-  {
-    name: "8号摄像头",
-    id: 8,
-    url:
-      "http://hls01open.ys7.com/openlive/3c3d8a4396ff49018e7a1011095a22b9.hd.m3u8"
-  },
-  {
-    name: "9号摄像头",
-    id: 9,
-    url:
-      "http://hls01open.ys7.com/openlive/3c3d8a4396ff49018e7a1011095a22b9.hd.m3u8"
-  },
-  {
-    name: "10号摄像头",
-    id: 10,
-    url:
-      "http://hls01open.ys7.com/openlive/3c3d8a4396ff49018e7a1011095a22b9.hd.m3u8"
-  },
-  {
-    name: "11号摄像头",
-    id: 11,
-    url:
-      "http://hls01open.ys7.com/openlive/3c3d8a4396ff49018e7a1011095a22b9.hd.m3u8"
-  },
-  {
-    name: "12号摄像头",
-    id: 12,
-    url:
-      "http://hls01open.ys7.com/openlive/3c3d8a4396ff49018e7a1011095a22b9.hd.m3u8"
-  },
-  {
-    name: "13号摄像头",
-    id: 13,
-    url:
-      "http://hls01open.ys7.com/openlive/3c3d8a4396ff49018e7a1011095a22b9.hd.m3u8"
-  },
-  {
-    name: "14号摄像头",
-    id: 14,
-    url:
-      "http://hls01open.ys7.com/openlive/3c3d8a4396ff49018e7a1011095a22b9.hd.m3u8"
-  },
-  {
-    name: "15号摄像头",
-    id: 15,
-    url:
-      "http://hls01open.ys7.com/openlive/3c3d8a4396ff49018e7a1011095a22b9.hd.m3u8"
-  },
-  {
-    name: "16号摄像头",
-    id: 16,
-    url:
-      "http://hls01open.ys7.com/openlive/3c3d8a4396ff49018e7a1011095a22b9.hd.m3u8"
-  }
-];
+import msg from "common/MessageUtils"
 
 @Component({})
 export default class VideoPage extends Vue {
@@ -113,10 +16,18 @@ export default class VideoPage extends Vue {
   private videoSize = 4; // 每页显示的视频数量
   private pageIndex = 1; // 页码
   private videoTotal = 0; // 摄像头总数
-  private videoList: any[] = videoData; // 摄像头列表
+  private videoList: any[] = []; // 摄像头列表
   // private cid = "76"
   private cid = "1";
   private videoShow = true; // 重绘页面
+  private menuList: any[] = []; // 菜单列表
+  private tag = true; // true 集团 false 项目
+  private defaultUrl = require("static/lodaingFail.jpg");
+  private props = {
+    label: "companyName",
+    children: "children",
+    isLeaf: "leaf"
+  };
 
   constructor() {
     super();
@@ -125,6 +36,7 @@ export default class VideoPage extends Vue {
 
   mounted() {
     this.getVideoList();
+    this.getMenuList();
   }
 
   // 翻页
@@ -145,7 +57,7 @@ export default class VideoPage extends Vue {
     this.videoShow = false;
     this.store
       .getVideoList(
-        `?cid=${this.cid}&pageNum=${this.pageIndex}&pageSize=${this.videoSize}`
+        `?cid=${this.cid}&pageNum=${this.pageIndex}&pageSize=${this.videoSize}&tag=${this.tag}`
       )
       .then((res: any) => {
         if (res.code === "0") {
@@ -176,6 +88,59 @@ export default class VideoPage extends Vue {
       });
       resolve(true);
     });
+  }
+
+  // 获取点击列表
+  getMenuList() {
+    this.store
+      .getMenuList(`?id=${this.cid}&tag=${this.tag}&type=video`)
+      .then((res: any) => {
+        if (res.code === "0") {
+          this.menuList = res.data;
+        }
+      });
+  }
+
+  // 获取项目数据
+  getProject(id: number): Promise<any> {
+    return new Promise(resolve => {
+      this.store.getProject(`?companyId=${id}`).then((res: any) => {
+        if (res.code === "0" && res.data.length) {
+          res.data.forEach((project: any) => {
+            project.companyName = project.projectName;
+            project.leaf = true;
+          });
+          resolve(res.data);
+          return
+        }
+        msg.warning("该公司下没有项目")
+        resolve([]);
+      });
+    });
+  }
+
+  // 树形的渲染
+  async loadNode(node: any, resolve: any) {
+    if (node.level === 0) {
+      return resolve(this.menuList);
+    }
+
+    const childList = this.menuList.filter(
+      (item: any) => item.id === node.data.id
+    );
+    if (childList.length && childList[0].children) {
+      return resolve(childList[0].children);
+    }
+
+    const data = await this.getProject(node.data.id);
+    return resolve(data);
+  }
+
+  // 播放项目视频
+  getProjectVideo(id: string) {
+    this.cid = id;
+    this.tag = false;
+    this.getVideoList();
   }
 
   beforeDestroy() {
